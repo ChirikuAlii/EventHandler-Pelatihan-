9e2a323e0c0765c0794b6c6a32862790e97275fb
alirhmat <chiriku.ali.rhmat@gmail.com>
2017-12-16 19:56:14 +0700

    connect xml with java

--- app/src/main/java/com/example/alirahmat/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alirahmat/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.alirahmat.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtA,scoreA, txtB , scoreB ;
+    Button upA,downA , upB , downB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        txtA = (TextView) findViewById(R.id.txt_a);
+        scoreA = (TextView) findViewById(R.id.txt_score_a);
+
+
     }
 }
